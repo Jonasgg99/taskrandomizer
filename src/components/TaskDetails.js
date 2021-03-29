057ddaa973e9f984c7bfe1fb8326775556d1b7f1
Jonas <jonaseg@gmail.com>
2021-03-29 17:46:12 +0200

    add Complete button to tasks

--- src/components/TaskDetails.js
+++ src/components/TaskDetails.js
@@ -2,14 +2,42 @@ import React from 'react'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button'
+
+const CompleteButton = () => {
+  const [completed, setCompleted] = React.useState(false)
+
+  const completeTask = () => {
+    setCompleted(true)
+  }
+  
+  if (!completed) {
+    return (
+      <Button 
+      onClick={completeTask}
+      color="primary"
+      variant="contained"
+      >Complete</Button>
+    )
+  } else {
+    return (
+      <Button 
+      disabled
+      variant="contained"
+      color="primary"
+      >Completed!</Button>
+    )
+  }
+}
 
 const TaskDetails = (props) => {
-
+  
   return (
     <Paper>
-    <ListItem>
-      <ListItemText primary={props.task} secondary={props.category} />
-    </ListItem>
+      <ListItem>
+        <ListItemText primary={props.task} secondary={props.category} />
+        <CompleteButton />
+      </ListItem>
     </Paper>
   )
 }
