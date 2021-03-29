c24853fb55fc8a411cc63f71eafbe14b576594f4
Jonasgg99 <73074277+Jonasgg99@users.noreply.github.com>
2021-03-29 13:21:07 +0200

    add temp data; add paper look to tasks

--- src/components/TaskDetails.js
+++ src/components/TaskDetails.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
+import Paper from '@material-ui/core/Paper'
 
 const TaskDetails = (props) => {
 
   return (
+    <Paper>
     <ListItem>
       <ListItemText primary={props.task} secondary={props.category} />
     </ListItem>
+    </Paper>
   )
 }
 
--- src/reducers/categoryReducer.js
+++ src/reducers/categoryReducer.js
@@ -1,8 +1,14 @@
 
+const categories = [
+  {name:'Inntekt', tasks:['kontakt arbeidsgiver','skriv søknad','redigere artikkel på snl']},
+  {name:'Kreativ', tasks:['filme scene', 'skrive script','redigere','skrive']},
+  {name:'Utleie', tasks:['vaske klær', 'vaske do','rydde i et skap','ta ned gardiner','vask dusj']},
+  {name:'Kompetanseutvikling', tasks:['FullStack kursing','Jobbe på app:taskrandomizer', 'Jobbe på app:spisopp','WebSec-kurs','Undersøke master']},
+  {name:'Sunnhet', tasks:['lage mat m betak','kroppslig stell','klippe hår']},
+  
+]
 
-const categoryReducer = (state = [
-  {name:'Training', tasks:['pullups','pushups','look']}, 
-  {name:'Finance', tasks:['work','apply', 'test']}], 
+const categoryReducer = (state = categories, 
   action) => {
   switch(action.type) {
     default:
